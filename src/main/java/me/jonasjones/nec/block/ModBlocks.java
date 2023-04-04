14b2e44abb3c67e2a90fdc5f7b5637aa934307f8
Jonas_Jones <91549607+J-onasJones@users.noreply.github.com>
2023-04-05 00:16:29 +0200

    added pocket block

--- src/main/java/me/jonasjones/nec/block/ModBlocks.java
+++ src/main/java/me/jonasjones/nec/block/ModBlocks.java
@@ -18,10 +18,12 @@ public class ModBlocks {
     public static final Block DIAMOND_ORE_ORE = new Block(FabricBlockSettings.of(Material.STONE).strength(4.0F));
     public static final Block DEEPSLATE_DIAMOND_ORE_ORE = new Block(FabricBlockSettings.of(Material.STONE).strength(4.0F));
     public static final Block JAVA_BLOCK = new Block(FabricBlockSettings.of(Material.STONE).strength(5.0F));
+    public static final Block POCKET_BLOCK = new Block(FabricBlockSettings.of(Material.STONE).strength(5.0F));
     private static BlockItem GUN_BLOCK_ITEM;
     private static Block GUN_BLOCK_BLOCK;
     private static BlockItem BLAZE_BLOCK_ITEM;
     private static Block BLAZE_BLOCK_BLOCK;
+    public static BlockItem GREEN_BIRCH_LOG_ITEM;
     public static Block GREEEN_BIRCH_LOG_BLOCK;
     public static BlockItem DIAMOND_ORE_ORE_ITEM;
     public static Block DIAMOND_ORE_ORE_BLOCK;
@@ -29,7 +31,8 @@ public class ModBlocks {
     public static Block DEEPSLATE_DIAMOND_ORE_ORE_BLOCK;
     public static BlockItem JAVA_BLOCK_ITEM;
     public static Block JAVA_BLOCK_BLOCK;
-    public static BlockItem GREEN_BIRCH_LOG_ITEM;
+    public static BlockItem POCKET_BLOCK_ITEM;
+    public static Block POCKET_BLOCK_BLOCK;
     //public static final Block GREEN_BIRCH_WOOD = new PillarBlock(FabricBlockSettings.of(Material.WOOD).strength(4.0f));
     //public static final Block STRIPPED_GREEN_BIRCH_LOG = new PillarBlock(FabricBlockSettings.of(Material.WOOD).strength(4.0f));
     //public static final Block STRIPPED_GREEN_BIRCH_WOOD = new PillarBlock(FabricBlockSettings.of(Material.WOOD).strength(4.0f));
@@ -58,5 +61,9 @@ public class ModBlocks {
         JAVA_BLOCK_BLOCK = Registry.register(Registries.BLOCK, new Identifier("nec", "java_block"), JAVA_BLOCK);
         JAVA_BLOCK_ITEM = Registry.register(Registries.ITEM, new Identifier("nec", "java_block"), new BlockItem(JAVA_BLOCK, new FabricItemSettings()));
         ModRegistries.register_item(JAVA_BLOCK_ITEM);
+
+        POCKET_BLOCK_BLOCK = Registry.register(Registries.BLOCK, new Identifier("nec", "pocket_block"), POCKET_BLOCK);
+        POCKET_BLOCK_ITEM = Registry.register(Registries.ITEM, new Identifier("nec", "pocket_block"), new BlockItem(POCKET_BLOCK, new FabricItemSettings()));
+        ModRegistries.register_item(POCKET_BLOCK_ITEM);
     }
 }
