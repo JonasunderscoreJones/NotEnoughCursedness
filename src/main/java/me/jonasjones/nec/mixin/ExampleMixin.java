664a6f9b6787e0959f6790b597f75e366120c818
Jonas_Jones <91549607+J-onasJones@users.noreply.github.com>
2023-04-04 04:28:44 +0200

    initial project structure

---
+++ src/main/java/me/jonasjones/nec/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package me.jonasjones.nec.mixin;
+
+import me.jonasjones.nec.NotEnoughCursedness;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		NotEnoughCursedness.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
